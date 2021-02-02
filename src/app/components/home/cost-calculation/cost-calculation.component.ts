fc91ae5a665b5fe41b840e6f8695f2dca5413177
AlexHamburg <oleksandr.trunov@gmail.com>
2021-02-02 09:25:16 +0100

    added utility service, inputs validation

--- src/app/components/home/cost-calculation/cost-calculation.component.ts
+++ src/app/components/home/cost-calculation/cost-calculation.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilityService } from '../../../services/utility.service';
 
 @Component({
   selector: 'app-cost-calculation',
@@ -8,22 +9,31 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class CostCalculationComponent {
   costCalculationForm: FormGroup;
+
+  private static readonly INPUT_REGEX_DIGIT: string = '^[0-9]+(.|,)?[0-9]*$';
   
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private utilityService: UtilityService) {
     this.costCalculationForm = this.formBuilder.group({
-      carPrice: [''],
-      owningPeriod: [''],
-      kmInYear: [''],
-      cleanCarInYear: [''],
-      cleanCarPrice: [''],
-      insurancePrice: [''],
-      lossOfValue: [''],
-      taxYear: [''],
-      fuelConsumptionKm: [''],
-      opportunityCost: [''],
-      parkplaceCost: [''],
-      countInspectionsInYear: [''],
-      inspectionPrice: ['']
+      carPrice: ['', [Validators.required, Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      owningPeriod: ['', 
+        [
+          Validators.required, 
+          Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT), 
+          Validators.minLength(1)
+        ]
+      ],
+      kmInYear: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      cleanCarInYear: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      cleanCarPrice: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      insurancePrice: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      lossOfValue: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      taxYear: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      fuelConsumptionKm: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      opportunityCost: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      parkplaceCost: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      countInspectionsInYear: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]],
+      inspectionPrice: ['', [Validators.pattern(CostCalculationComponent.INPUT_REGEX_DIGIT)]]
     });
    }
 
