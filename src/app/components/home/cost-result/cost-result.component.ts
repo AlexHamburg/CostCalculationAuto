9f3a5d23e8bbb15bdf57c0303fb3e9235f79974c
AlexHamburg <oleksandr.trunov@gmail.com>
2021-02-15 09:38:46 +0100

    added pdf export

--- src/app/components/home/cost-result/cost-result.component.ts
+++ src/app/components/home/cost-result/cost-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { jsPDF } from 'jspdf';
 import { CalculationResult } from '../cost-calculation/cost-calculation.component';
 
 @Component({
@@ -13,4 +14,16 @@ export class CostResultComponent {
     totalCost: 0,
     yearCost: 0,
   };
+
+  exportPDF(): void {
+    var doc = new jsPDF();
+    doc.setFontSize(22);
+    doc.text('Ergebnis:', 20, 20);
+    doc.setFontSize(16);
+    doc.text(`Kosten pro km.: ${this.calculResult.kmCost}`, 20, 60);
+    doc.text(`Kosten pro Monat: ${this.calculResult.monthCost}`, 20, 80);
+    doc.text(`Kosten pro Jahr: ${this.calculResult.yearCost}`, 20, 100);
+    doc.text(`Gesamtkosten: ${this.calculResult.totalCost}`, 20, 120);
+    doc.save('data.pdf');
+  }
 }
