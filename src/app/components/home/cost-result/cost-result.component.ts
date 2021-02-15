4e87ae36deb9377d4d24c61fb0b3d0d8ba06a1b2
AlexHamburg <oleksandr.trunov@gmail.com>
2021-02-15 09:57:17 +0100

    added btn pdf export

--- src/app/components/home/cost-result/cost-result.component.ts
+++ src/app/components/home/cost-result/cost-result.component.ts
@@ -15,7 +15,7 @@ export class CostResultComponent {
     yearCost: 0,
   };
 
-  exportPDF(): void {
+  exportPDF() {
     var doc = new jsPDF();
     doc.setFontSize(22);
     doc.text('Ergebnis:', 20, 20);
@@ -24,6 +24,6 @@ export class CostResultComponent {
     doc.text(`Kosten pro Monat: ${this.calculResult.monthCost}`, 20, 80);
     doc.text(`Kosten pro Jahr: ${this.calculResult.yearCost}`, 20, 100);
     doc.text(`Gesamtkosten: ${this.calculResult.totalCost}`, 20, 120);
-    doc.save('data.pdf');
+    doc.save('resultCostCalculation.pdf');
   }
 }
